6b036f6802af073af572f74eecc6e368847dcd7b
Helen <e.n.komarova@yandex.ru>
2023-01-04 19:46:45 +0300

    HomeWork One corrected version

--- HomeWorkOne/Program.cs
+++ HomeWorkOne/Program.cs
@@ -43,7 +43,7 @@ else
 Console.WriteLine("An odd number");
 */
 
-//Задача 4. Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+/*Задача 4. Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 Console.Write ("Input a  number: ");
 int num = Convert.ToInt32(Console.ReadLine());
 int current = 1;
@@ -52,3 +52,4 @@ while (current <= num)
 Console.Write( current +" " );
 current ++;
 }
+*/
